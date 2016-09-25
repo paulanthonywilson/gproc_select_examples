4b7e7985e1c400556b859be630b48cb4fe9343d6
Paul Wilson <paulanthonywilson@gmail.com>
2016-09-25 12:22:27 +0100

    Use `ets:fun2ms` to create the matchers

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Just getting to grips with [Gproc's select](https://github.com/uwiger/gproc/blob/master/doc/gproc.md#select-1) by translating [these examples](https://gist.github.com/rustyio/188032) from Erlang to Elixir.
 
+Updated to use [`ets.fun2ms`](http://erlang.org/doc/man/ets.html#fun2ms-1) as [suggested by Saša Jurić](https://twitter.com/sasajuric/status/779610386832162816)
+
 ## Getting setup
 
 ```
--- test/gproc_select_test.exs
+++ test/gproc_select_test.exs
@@ -1,9 +1,13 @@
 defmodule GprocSelectTest do
   use ExUnit.Case
-  doctest GprocSelect
+
+  @compile {:parse_transform, :ms_transform}
 
   @moduledoc """
-  Translation of https://gist.github.com/rustyio/188032
+  Translation of https://gist.github.com/rustyio/188032 to Elixir, and using
+  `ets.fun2ms` to create the matchers.
+
+  (Using `fun2ms` suggested by Saša Jurić https://twitter.com/sasajuric/status/779610386832162816 )
   """
 
   setup do
@@ -16,58 +20,68 @@ defmodule GprocSelectTest do
   end
 
   test "selecting all" do
-    match_head = :"_"
-    guard = []
-    result = [:"$$"]
+    matcher = :ets.fun2ms(fn x -> x end)
+
+    assert [{:"$1", [], [:"$1"]}] == matcher
+
     assert [
-      [{:n, :l, :key1}, self, :value1],
-      [{:n, :l, :key2}, self, :value2],
-      [{:n, :l, :key3}, self, :value3],
-      [{:n, :l, 'list key'}, self, 'list value'],
-      [{:n, :l, [:complex, :key]}, self, [:complex, :value]]
-    ] == :gproc.select([{match_head, guard, result}])
+      {{:n, :l, :key1}, self, :value1},
+      {{:n, :l, :key2}, self, :value2},
+      {{:n, :l, :key3}, self, :value3},
+      {{:n, :l, 'list key'}, self, 'list value'},
+      {{:n, :l, [:complex, :key]}, self, [:complex, :value]}
+    ] == :gproc.select(matcher)
   end
 
   test "select by value" do
-    match_head = {:"_", :"_", :value1}
-    guard = []
-    result = [:"$$"]
+    matcher = :ets.fun2ms(fn {_, _, :value1} = x -> x end)
+
+    assert [{{:_, :_, :value1}, [], [:"$_"]}] == matcher
+
     assert [
-      [{:n, :l, :key1}, self, :value1],
-    ] == :gproc.select([{match_head, guard, result}])
+      {{:n, :l, :key1}, self, :value1},
+    ] == :gproc.select(matcher)
   end
 
   test "match a complex key" do
-    key = [:complex, :"_"]
-    gproc_key = {:"_", :"_", key}
-    match_head = {gproc_key, :"_", :"_"}
-    guard = []
-    result = [:"$$"]
+    matcher = :ets.fun2ms(fn {{_, _, [:complex, _]}, _, _} = x -> x end)
+
+    assert [{{{:_, :_, [:complex, :_]}, :_, :_}, [], [:"$_"]}] == matcher
+
     assert [
-      [{:n, :l, [:complex, :key]}, self, [:complex, :value]]
-    ] == :gproc.select([{match_head, guard, result}])
+      {{:n, :l, [:complex, :key]}, self, [:complex, :value]}
+    ] == :gproc.select(matcher)
   end
 
   test "using a guard to find entries with keys that are lists" do
-    gproc_key = {:"_", :"_", :"$1"}
-    match_head = {gproc_key, :"_", :"_"}
-    guard = [{:is_list, :"$1"}]
-    result = [:"$$"]
+    matcher = :ets.fun2ms(fn {{_, _, key}, _, _} = x when is_list(key) -> x end)
+
+    assert [{{{:_, :_, :"$1"}, :_, :_}, [is_list: :"$1"], [:"$_"]}] == matcher
+
     assert [
-      [{:n, :l, 'list key'}, self, 'list value'],
-      [{:n, :l, [:complex, :key]}, self, [:complex, :value]]
-    ] == :gproc.select([{match_head, guard, result}])
+      {{:n, :l, 'list key'}, self, 'list value'},
+      {{:n, :l, [:complex, :key]}, self, [:complex, :value]}
+    ] == :gproc.select(matcher)
   end
 
   test "a more complicated guard: everything that is a list longer than 5 items" do
-    gproc_key = {:"_", :"_", :"$1"}
-    match_head = {gproc_key, :"_", :"_"}
-    guard = [
-      {:"andalso", {:is_list, :"$1"}, {:">", {:length, :"$1"}, 5}},
-    ]
-    result = [:"$$"]
+    matcher = :ets.fun2ms(fn {{_, _, key}, _, _} = x when is_list(key) and length(key) > 5 -> x end)
+
+    assert [{{{:_, :_, :"$1"}, :_, :_},
+      [{:andalso, {:is_list, :"$1"}, {:>, {:length, :"$1"}, 5}}],
+      [:"$_"]}] == matcher
+
+
     assert [
-      [{:n, :l, 'list key'}, self, 'list value'],
-    ] == :gproc.select([{match_head, guard, result}])
+      {{:n, :l, 'list key'}, self, 'list value'},
+    ] == :gproc.select(matcher)
+  end
+
+  test "just returning the stored process" do
+    matcher = :ets.fun2ms(fn {_, pid, :value1} -> pid end)
+
+    assert [{{:_, :"$1", :value1}, [], [:"$1"]}] == matcher
+
+    assert [self] == :gproc.select(matcher)
   end
 end
